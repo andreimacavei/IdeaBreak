e37f286c6b1a9302df32b5175bbbb05116597458
andreimacavei <andrei.macavei89@gmail.com>
2024-01-22 16:03:18 +0200

    Added config and models folders

---
+++ models/Idea.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const IdeaSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, 'Please add some text'],
+  },
+  tag: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: new Date().toISOString().slice(0, 10),
+  },
+});
+
+module.exports = mongoose.model('Idea', IdeaSchema);
